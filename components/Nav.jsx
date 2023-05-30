6e7d4d22e4a55764187aa88b952d9e201a9634ac
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-05-30 14:17:24 +0200

    add subabase Client GET function, nav

---
+++ components/Nav.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import avatar from '@/public/avatar.png'
+
+const Nav = () => {
+  return (
+    <nav className='p-4 flex justify-between items-center'>
+        <div>
+            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="1" y="15" width="10" height="10" rx="3" stroke="#5E5E5E" strokeWidth="2"/>
+                <rect x="1" y="1" width="10" height="10" rx="3" stroke="#5E5E5E" strokeWidth="2"/>
+                <rect x="15" y="1" width="10" height="10" rx="3" stroke="#5E5E5E" strokeWidth="2"/>
+                <rect x="15" y="15" width="10" height="10" rx="3" stroke="#5E5E5E" strokeWidth="2"/>
+            </svg>
+        </div>
+        <h1 className='text-2xl text-lightGrey font-black'>rytm</h1>
+        <Image src={avatar} alt="Profile Avatar"/>
+    </nav>
+  )
+}
+
+export default Nav
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        accentGreen: '#E7FAAA',
+        accentPurple: '#B89BDF',
+        lightColor: '#FFFFFF',
+        darkColor: '#232323',
+        darkGrey: '#454545',
+        lightGrey: '#696969',
       },
     },
   },
